e37f79f1f220b3a6b5976cf910020aebb8c9acb9
yahyacodes <yahya.codin@gmail.com>
2024-01-16 12:34:09 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -9,21 +9,21 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
+    
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
-
+    
     def post(self):
+
         data = request.get_json()
 
         new_plant = Plant(
@@ -36,20 +36,16 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
-
-
+    
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-
 api.add_resource(PlantByID, '/plants/<int:id>')
-
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
